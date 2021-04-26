8b1df9b910f2808536213a02d5a4218f070993f0
Vic <Vic@gmail.com>
2021-04-26 10:54:09 +0800

    deploy

---
+++ js/admin.js
@@ -0,0 +1,422 @@
+/* get DOM */
+const ordersList = document.querySelector('.js-ordersList') ;
+const tokenDisplay = document.querySelector('.js-tokenDisplay') ;
+const tokenInput = document.querySelector('.js-tokenInput') ;
+const pathInput = document.querySelector('.js-pathInput') ;
+const tokenSubmit = document.querySelector('.js-tokenSubmit') ;
+const errorMsg = document.querySelector('.js-errorMsg') ;
+const orderDisplay = document.querySelector('.js-orderDisplay') ;
+const chartsDisplay = document.querySelector('.js-chartsDisplay') ;
+const delAllBtn = document.querySelector('.js-delAllBtn') ;
+const signOut = document.querySelector('.js-signOut') ;
+
+/* set 變數與初始值 */
+const apiUrl = 'https://hexschoollivejs.herokuapp.com/api/livejs/v1/admin' ;
+let token = '' ;
+let apiPath = '' ;
+let originOrdersData = [] ;
+let totalProductsData = {} ;
+let uuid = {}
+
+/* function */
+// get Data
+const getOrder = () => { 
+  axios.get(`${apiUrl}/${apiPath}/orders`, uuid)
+  .then(response => {
+    tokenDisplay.classList.add('d-none') ;
+    orderDisplay.classList.remove('d-none') ;
+    originOrdersData = response.data.orders ;
+    if (originOrdersData[0] === undefined) {
+      defaultNotice('warning', '還沒有訂單喔!') ;
+    } else {
+      chartsDisplay.classList.remove('d-none') ;
+      highChartRender(originOrdersData) ;
+    }
+    ordersRender(originOrdersData) ;
+  }).catch(() => {
+    tokenDisplay.classList.remove('d-none') ;
+    errorMsg.textContent = 'Input Error' ;
+    document.cookie = 'hexToken=; expires=; path=/' ;
+    document.cookie = 'hexPath=; expires=; path=/' ;
+    defaultNotice('error', '登入失敗！')
+  })
+}
+
+const getProducts = () => {
+  axios.get(`https://hexschoollivejs.herokuapp.com/api/livejs/v1/customer/${apiPath}/products`)
+  .then(response => {
+    let productsData = response.data.products ;
+    // 列出所有商品 ID，以利後續圖表整合
+    productsData.forEach(item => {
+      if (totalProductsData[item.id] === undefined){
+        totalProductsData[item.id] = {
+          title: item.title,
+          price: item.price,
+          category: item.category,
+          totalQuantity: 0
+        }
+      }
+    })
+  }).catch(() => {
+    tokenDisplay.classList.remove('d-none') ;
+    errorMsg.textContent = 'Input Error' ;
+    document.cookie = 'hexToken=; expires=; path=/' ;
+    document.cookie = 'hexPath=; expires=; path=/' ;
+  })
+}
+
+// put Data
+const putOrder = (orderID, bool) => { 
+  let orderData = {data:{id: orderID, paid: bool}}
+  axios.put(`${apiUrl}/${apiPath}/orders`, orderData, uuid).then(response => {
+    ordersRender(response.data.orders) ;
+  }).catch(()=>{
+    defaultNotice('warning', '修改訂單狀態失敗!')
+  })
+}
+
+// delete Data
+const deleteOrder = ordersID => { 
+  if (ordersID === 'clearAll' && ordersList.textContent === '') {
+    defaultNotice('warning', '沒有訂單呦~')
+  } else if (ordersID === 'clearAll' && ordersList.textContent !== '') {
+    axios.delete(`${apiUrl}/${apiPath}/orders`, uuid).then(response => {
+      chartsDisplay.classList.add('d-none') ;
+      ordersRender(response.data.orders) ;
+      highChartRender(response.data.orders) ;
+      defaultNotice('success', '已全部刪除!')
+    }).catch(() => {
+      defaultNotice('error', '刪除失敗~')
+    })
+  } else {
+    axios.delete(`${apiUrl}/${apiPath}/orders/${ordersID}`,  uuid).then(response => {
+      ordersRender(response.data.orders) ;
+      highChartRender(response.data.orders) ;
+      if (response.data.orders[0] === undefined) {
+        chartsDisplay.classList.add('d-none') ;
+      }
+      defaultNotice('success', '已成功刪除!') ;
+    }).catch(() => {
+      defaultNotice('error', '刪除失敗~') ;
+    })
+  }
+}
+
+const ordersRender = data => { 
+  let dataStr = '' ;
+  data.sort((a, b) => {
+    return b.createdAt - a.createdAt
+  })
+  data.forEach((item, index) => {
+    let otherProductsStr = '' ;
+    let moreStr = '' ;
+    if (item.products[1]) {
+      moreStr = `
+      <a class="btn btn-primary" data-bs-toggle="collapse" href="#collapse${index}" role="button" aria-expanded="false" aria-controls="collapse${index}">
+        more
+      </a>`
+    }
+    item.products.forEach((productItem, index) => {
+      if (item.products[1] && index !== 0)
+      otherProductsStr += `<p class="mb-0">${productItem.title}</p>` ;
+    }) 
+    dataStr += `
+    <tr>
+      <td>${item.id}</td>
+      <td>
+        <p class="mb-0">${item.user.name}</p>
+        <p class="mb-0">${item.user.tel}</p>
+      </td>
+      <td>${item.user.address}</td>
+      <td>${item.user.email}</td>
+      <td>
+        ${item.products[0].title}
+        <div class="collapse" id="collapse${index}">
+          ${otherProductsStr}
+        </div>
+      </td>
+      <td>${formatDate(item.createdAt*1000)}</td>
+      <td class="bg-dark text-white">
+        <div class="d-flex flex-column align-items-center">
+          <label class="switch me-2">
+            <input type="checkbox" data-orderID="${item.id}" ${item.paid ? 'checked' : ''} >
+            <span class="slider round"></span>
+          </label>
+          <span>${item.paid ? '已處理' : '未處理'}</span> 
+        </div>
+      </td>
+      <td class="text-end">
+        ${moreStr}
+        <button type="button" value="${item.id}" class="js-delBtn btn btn-outline-danger">刪除</button>
+      </td>
+    </tr>   
+    `
+  }) 
+  if (dataStr === '') {
+    delAllBtn.classList.add('d-none') ;
+    ordersList.innerHTML = `<tr><td colspan="9" class="text-center">還沒有訂單喔~</td></tr>` ;
+  } else {
+    delAllBtn.classList.remove('d-none') ;
+    ordersList.innerHTML = dataStr ;
+  }
+}
+
+// highChart.js
+const highChartRender = orderData => {
+  // 全部資料暫存
+  let tempData = [] ;
+  let tempTotalProuctsData = JSON.parse(JSON.stringify(totalProductsData))
+  
+  // 前三名資料暫存
+  let topThreeData = [] ;
+  let tempOtherData = {name:'其他', y: 0} ;
+
+  // 類別資料暫存
+  let tempCategoryData = []
+  let tempCategoryObj = {}
+
+  // 將訂單資料的商品 ID 數量整合並做營收遞減排序
+  orderData.forEach(orderItem => {
+    orderItem.products.forEach(item => {
+      tempTotalProuctsData[item.id].totalQuantity += item.quantity ;
+    })
+  })
+  tempData = Object.keys(tempTotalProuctsData) ;
+  tempData.map((item, index) =>{
+    tempData[index] = {
+      name: tempTotalProuctsData[item].title, 
+      y: tempTotalProuctsData[item].price * tempTotalProuctsData[item].totalQuantity,
+      category:tempTotalProuctsData[item].category 
+    }
+    return
+  })
+  tempData.sort((a, b) => {
+    return b.y - a.y
+  })
+
+  // 取出前三名資料與其他
+  tempData.forEach((item, index) => {
+    if (index < 3) {
+      topThreeData.push(item)
+    } else {
+      tempOtherData.y += item.y
+    }
+  })
+  topThreeData.push(tempOtherData)
+  
+  // 類別營收
+  tempData.forEach(item => {
+    if (tempCategoryObj [item.category] === undefined) {
+      tempCategoryObj[item.category] = item.y
+    } else {
+      tempCategoryObj[item.category] += item.y
+    }
+  })
+  tempCategoryData = Object.keys(tempCategoryObj)
+  tempCategoryData.map((item, index) => {
+    tempCategoryData[index] = {name:item, y:tempCategoryObj[item]}
+    return
+  }) 
+  Highcharts.chart('highChart', {
+    chart: {
+      plotBackgroundColor: null,
+      plotBorderWidth: null,
+      plotShadow: false,
+      type: 'pie'
+    },
+    title: {
+      text: `全品項營收`
+    },
+    tooltip: {
+      // 格式化為 % 數
+      // pointFormat: '{series.name}:<b>{point.percentage:.1f}%</b>'
+    },
+    colors: ['#3366CC', '#DC3912', '#FF9900', '#109618', '#990099', '#3B3EAC', '#0099C6', '#DD4477', '#66AA00', '#B82E2E', '#316395', '#994499', '#22AA99', '#AAAA11', '#6633CC', '#E67300', '#8B0707', '#329262', '#5574A6', '#3B3EAC'],
+    plotOptions: {
+      pie: {
+        allowPointSelect: true,
+        innerSize: '50%',
+        cursor: 'pointer',
+        dataLabels: {
+            enabled: true,
+            format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+            connectorColor: 'black'
+        }
+      }
+    },
+    xAxis: {
+      categories: Object.keys(totalProductsData)
+    },
+    series: [{
+      name: '實際營收',
+      data: tempData
+    }],
+    credits: {
+      enabled: false
+  	}
+  });
+  Highcharts.chart('highChartTopThree', {
+    chart: {
+      plotBackgroundColor: null,
+      plotBorderWidth: null,
+      plotShadow: false,
+      type: 'pie'
+    },
+    title: {
+      text: '前三名品項營收'
+    },
+    colors: ['#301E5F', '#5434A7', '#9D7FEA', '#DACBFF'],
+    plotOptions: {
+      pie: {
+        allowPointSelect: true,
+        innerSize: '50%',
+        cursor: 'pointer',
+        dataLabels: {
+            enabled: true,
+            format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+            connectorColor: 'black'
+        }
+      }
+    },
+    xAxis: {
+      categories: Object.keys(totalProductsData)
+    },
+    series: [{
+      name: '實際營收',
+      data: topThreeData
+    }],
+    credits: {
+      enabled: false
+  	}
+  }) ;
+  Highcharts.chart('highChartCategory', {
+    chart: {
+      plotBackgroundColor: null,
+      plotBorderWidth: null,
+      plotShadow: false,
+      type: 'pie'
+    },
+    title: {
+      text: '類別總營收'
+    },
+    colors: ['#301E5F', '#5434A7', '#9D7FEA', '#DACBFF'],
+    plotOptions: {
+      pie: {
+        allowPointSelect: true,
+        innerSize: '50%',
+        cursor: 'pointer',
+        dataLabels: {
+            enabled: true,
+            format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+            connectorColor: 'black'
+        }
+      }
+    },
+    xAxis: {
+      categories: Object.keys(totalProductsData)
+    },
+    series: [{
+      name: '實際營收',
+      data: tempCategoryData
+    }],
+    credits: {
+      enabled: false
+  	}
+  }) ;
+}
+
+// 資料給的是秒數，而非毫秒數
+const formatDate = time => {
+  let date = new Date(time) ;
+  return `${date.getFullYear()}/${date.getMonth()+1}/${date.getDate()}` ;
+}
+
+const defaultNotice = (iconName, content) => { 
+  Swal.fire({
+    position: 'center',
+    icon: iconName,
+    title: content,
+    showConfirmButton: false,
+    timer: 1500
+  })
+}
+
+// 將 token 存入 cookie
+tokenSubmit.addEventListener('click', () => {
+  if (pathInput.value !== '' && tokenInput.value !== ''){
+    token = tokenInput.value.trim() ;
+    apiPath = pathInput.value.trim() ;
+    uuid = {headers:{Authorization:token}}
+    document.cookie = `hexToken=${token}; expires=${new Date().getTime()*1000}; path=/` ;
+    document.cookie = `hexPath=${apiPath}; expires=${new Date().getTime()*1000}; path=/` ;
+    tokenInput.value = '' ;
+    pathInput.value = '' ;
+    getOrder() ;
+    getProducts() ;
+  } else if (pathInput.value === '' && tokenInput.value === '') {
+    defaultNotice('warning', '請輸入 Path 與 Token')
+  } else if (pathInput.value === '') {
+    defaultNotice('warning', '請輸入 Path')
+  } else if (tokenInput.value === '') {
+    defaultNotice('warning', '請輸入 Token')
+  }
+})
+
+// 暫存 patch 與 token
+tokenInput.addEventListener('keydown', Event => {
+  if (Event.key === 'Enter' && pathInput.value !== '' && tokenInput.value !== ''){
+    token = tokenInput.value ;
+    apiPath = pathInput.value ;
+    uuid = {headers:{Authorization:token}}
+    document.cookie = `hexToken=${token}; expires=${new Date().getTime()*10}; path=/` ;
+    document.cookie = `hexPath=${apiPath}; expires=${new Date().getTime()*10}; path=/` ;
+    tokenInput.value = '' ;
+    getOrder() ;
+    getProducts() ;
+  } else if (Event.key === 'Enter' && pathInput.value === '' && tokenInput.value === '') {
+    defaultNotice('warning', '請輸入 Path 與 Token')
+  } else if (Event.key === 'Enter' && pathInput.value === '') {
+    defaultNotice('warning', '請輸入 Path') ;
+  } else if (Event.key === 'Enter' && tokenInput.value === '') {
+    defaultNotice('warning', '請輸入 Token') ;
+  }
+})
+
+
+// 登出並清除 cookie
+signOut.addEventListener('click', () => {
+  document.cookie = 'hexToken=; expires=; path=/' ;
+  document.cookie = 'hexPath=; expires=; path=/' ;
+  tokenDisplay.classList.remove('d-none') ;
+  orderDisplay.classList.add('d-none') ;
+  chartsDisplay.classList.add('d-none') ;
+  errorMsg.textContent = '' ;
+  originOrdersData = [] ;
+  totalProductsData = {} ;
+  token = '' ;
+  uuid = {} ;
+  ordersRender(originOrdersData) ;
+  highChartRender(originOrdersData) ;
+  window.location.replace('./index.html') ;
+})
+
+// 進入頁面時從 cookie 取出 token
+token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*=\s*([^;]*).*$)|^.*$/, '$1') ;
+apiPath = document.cookie.replace(/(?:(?:^|.*;\s*)hexPath\s*=\s*([^;]*).*$)|^.*$/, '$1') ;
+if (token && apiPath) {
+  // 如果被修改，這邊還會重組驗證用物件
+  uuid = {headers:{Authorization:token}}
+  getOrder() ;
+  getProducts() ;
+} else {
+  tokenDisplay.classList.remove('d-none') ;
+}
+
+orderDisplay.addEventListener('click', Event => {
+  if (Event.target.tagName === 'BUTTON') {
+    deleteOrder(Event.target.value) ;
+  }
+  if (Event.target.tagName === "INPUT") {
+    putOrder(Event.target.getAttribute('data-orderID'), Event.target.checked) ;
+  }
+}, true)
---
+++ js/all.js
@@ -0,0 +1,391 @@
+/* get DOM */
+const productsList = document.querySelector('.js-productsList') ;
+const productsCategory = document.querySelector('.js-productsCategory') ;
+const cartsList = document.querySelector('.js-cartsList') ;
+const delAll = document.querySelector('.js-delAll') ;
+const finalTotal = document.querySelector('.js-finalTotal') ;
+const submitOrder = document.querySelector('.js-submitOrder') ;
+const inputs = document.querySelectorAll("input[type=text],input[type=number],input[type=email],input[type=tel],select[name=clientPay]") ;
+const topBtn = document.querySelector('.js-top') ;
+
+/* set 變數與初始值 */
+const apiUrl = 'https://hexschoollivejs.herokuapp.com/api/livejs/v1/customer/vic'
+let originProductsData = [] ;
+let originCartsData = [] ;
+let hasError = false
+
+// validate 表單驗證條件與錯誤訊息 
+const constraints = {
+  clientName: {
+    presence: {
+      message: "請輸入姓名",
+    },
+    length: {
+      minimum: 2,
+      tooShort: "請勿輸入一個字",
+    }
+  },
+  clientPhone: {
+    presence: {
+      message: "請輸入電話",
+    },
+    length: {
+      minimum: 10,
+      tooShort: "請輸入正確的電話號碼",
+    },
+    format: {
+      pattern: "[0-9]+",
+      message: "請輸入正確的電話號碼",
+    }
+  },
+  clientEmail: {
+    presence: {
+      message: "請輸入電子信箱",
+    },
+    email: {
+      message: "請輸入正確的電子信箱",
+    },
+  },
+  clientAddress: {
+    presence: {
+      message: "請輸入寄送地址",
+    },
+    length: {
+      minimum: 10,
+      tooShort: "請輸入正確地址",
+    },
+  },
+  clientPay: {
+    presence: {
+      message: "請選擇交易方式",
+    },
+  }
+}
+
+/* function */
+// 資料初始化
+const init = () => {
+  getProducts();
+  getCarts();
+}
+
+// get Data
+const getProducts = () => {
+  axios.get(`${apiUrl}/products`).then(response => {
+    originProductsData = response.data.products ;
+    categoryOptions() ;
+    productsRender(originProductsData) ;
+  }).catch(() => {
+    defaultNotice('warning', '請重新整理頁面！')
+  })
+}
+const getCarts = () => {
+  axios.get(`${apiUrl}/carts`).then(response => {
+    originCartsData = response.data.carts ;
+    cartsRender(originCartsData, response.data.finalTotal) ;
+  }).catch(() => {
+    defaultNotice('warning', '請重新整理頁面！')
+  })
+}
+
+// post Data
+const postProduct = productID => {
+  let tempProduct = {data:{}} ;
+  let cartsHas = originCartsData.find(item => {
+    return item.product.id === productID ;
+  });
+  if (cartsHas === undefined) {
+    originProductsData.forEach(item => {
+      if (item.id === productID){
+        tempProduct.data.productId = productID ;
+        tempProduct.data.quantity = 1 ;
+      }
+    })
+    axios.post(`${apiUrl}/carts`, tempProduct).then(response => {
+      originCartsData = response.data.carts ;
+      defaultNotice('success', '已加入購物車')
+      cartsRender(originCartsData, response.data.finalTotal) ;
+    }).catch(() => {
+      defaultNotice('warning', '加入購物車失敗!')
+    })
+  } else {
+    defaultNotice('warning', '購物車已有相同商品!')
+  }
+}
+
+const postOrder = Event => {
+  let userObj = {} ;
+  userObj.name    = Event.target[0].value ;
+  userObj.tel     = Event.target[1].value ;
+  userObj.email   = Event.target[2].value ;
+  userObj.address = Event.target[3].value ;
+  userObj.payment = Event.target[4].value ;
+  // 若購物車為不為空且驗證成功
+  if (cartsList.textContent !== '' && !hasError){
+    axios.post(`${apiUrl}/orders`, {data:{user:userObj}}).then(() => {
+      cartsRender([], 0);
+      Event.target.reset()
+      originCartsData = []
+      defaultNotice('success', '感謝您的預定!')
+    }).catch(() => {
+      defaultNotice('error', '預定失敗~')
+    })
+  } else if (hasError) {
+    defaultNotice('warning', '請檢查預定資訊是否有誤!')
+  } else if (cartsList.textContent === '') {
+    defaultNotice('warning', '購物車沒有商品喔!')
+  }
+}
+
+// patch carts
+const patchProduct = (catrsID, newQuantity) => {
+  let tempProduct = {data:{}} ;
+  if (newQuantity === 0){
+    defaultNotice('warning', '數量不可低於 1 呦~') ;
+  } else {
+    tempProduct.data.id = catrsID ;
+    tempProduct.data.quantity = newQuantity ;
+    axios.patch(`${apiUrl}/carts`, tempProduct).then(response => {
+      cartsRender(response.data.carts, response.data.finalTotal) ;
+    }).catch(() => {
+      defaultNotice('warning', '更改數量失敗!')
+    })
+  }
+}
+
+// delete carts
+const deleteProduct = (cartsID) => {
+  if (cartsID === 'clearAll' && cartsList.textContent === '') {
+    defaultNotice('warning', '購物車沒有東西~請趕緊加入!')
+  } else if (cartsID === 'clearAll' && cartsList.textContent !== '') {
+    axios.delete(`${apiUrl}/carts`).then(() => {
+      originCartsData = []
+      cartsRender(originCartsData, 0) ;
+      defaultNotice('success', '已全部刪除!')
+    }).catch(() => {
+      defaultNotice('error', '刪除失敗~')
+    })
+  } else {
+    axios.delete(`${apiUrl}/carts/${cartsID}`).then(response => {
+      originCartsData = response.data.carts ;
+      cartsRender(originCartsData, response.data.finalTotal) ;
+      defaultNotice('success', '已成功刪除!')
+    }).catch(() => {
+      defaultNotice('error', '刪除失敗~')
+    })
+  }
+}
+
+// 渲染畫面
+// 產品清單
+const productsRender = data => {
+  let dataStr = '' ;
+  data.forEach(item => {
+    dataStr += `
+    <li class="col">
+      <div class="card border-0 rounded-0 h-100">
+        <div class="position-relative">
+          <img src="${item.images}" class="card-img-top" alt="${item.title}">
+          <p class="position-absolute top-0 end-0 translate-middle-y bg-secondary rounded-start p-3 text-white">新品</p>
+        </div>
+        <div class="card-body p-0">
+          <button type="button" value="${item.id}" class="js-addCartsBtn btn btn-dark rounded-0 w-100">加入購物車</button>
+          <h4 class="card-text text-info py-2 mb-0">${item.title}</h4>
+          <del>${formatPrice(item.origin_price)}</del>
+          <p class="h4">${formatPrice(item.price)}</p>
+        </div>
+      </div>
+    </li>` ;
+  })
+  productsList.innerHTML = dataStr ;
+  const addCartsBtns = document.querySelectorAll('.js-addCartsBtn') ;
+  addCartsBtns.forEach(item => {
+    item.addEventListener('click', Event => {
+      postProduct(Event.target.value) ;
+    })
+  })
+}
+
+// 類別清單
+const categoryOptions = () => { 
+  let selectOptions = '' ;
+  let tempCategory = {} ;
+  originProductsData.forEach(item => {
+    if (tempCategory[item.category] === undefined){
+      tempCategory[item.category] = 1 ;
+    } else {
+      tempCategory[item.category] += 1 ;
+    }
+  })
+  Object.keys(tempCategory).forEach(item => {
+    selectOptions += `<option value="${item}">${item}</option>` ;
+  })
+  productsCategory.innerHTML = '<option value="all" selected>全部</option>' + selectOptions ;
+}
+
+// 購物車清單
+const cartsRender = (data, totalMoney) => {
+  let dataStr = '' ;
+  data.forEach(item => {
+    dataStr += `
+    <tr>
+      <td>
+        <div class="d-flex align-items-center">
+          <img src="${item.product.images}" alt="" class="custom__carts__image me-2">
+          <p class="mb-0">${item.product.title}</p>
+        </div>
+      </td>
+      <td>${formatPrice(item.product.price)}</td>
+      <td>
+        <div class="d-flex justify-content-around align-items-center">
+          <button type="button" value="${item.id}" class="js-qtDown btn btn-light" data-action="down">-</button>
+          <button class="btn btn-light" data-quantity="${item.quantity}">${item.quantity}</button>
+          <button type="button" value="${item.id}" class="js-qtUp btn btn-light" data-action="up">+</button>
+        </div>
+      </td>
+      <td>${formatPrice(item.product.price * item.quantity)}</td>
+      <td><button type="button" value="${item.id}" class="js-delBtn btn btn-light" data-action="delete">X</button></td>
+    </tr>
+    ` ;
+  })
+  cartsList.innerHTML = dataStr ;
+  finalTotal.textContent = `NT$${formatPrice(totalMoney)}` ;
+}
+
+const defaultNotice = (iconName, content) => { 
+  Swal.fire({
+    position: 'center',
+    icon: iconName,
+    title: content,
+    showConfirmButton: false,
+    timer: 1500
+  })
+}
+
+// 數字千分位
+const formatPrice = numStr => {
+  return numStr.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") ;
+}
+
+
+// 是否顯示 top
+const showTop = () => {
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop
+  if (scrollTop > 100) {
+    topBtn.style.opacity = 1;
+  } else {
+    topBtn.style.opacity = 0 ;
+  }
+}
+
+// 預設執行
+init() ;
+window.addEventListener('scroll', showTop)
+
+/* 監聽事件 */
+// 在 DOM 結構被完整的讀取跟解析後，執行函式內動作
+document.addEventListener('DOMContentLoaded', () => {
+    const recommendDOM = document.querySelector('.custom__recommend') ;
+    // 變更滑鼠游標樣式
+    recommendDOM.style.cursor = 'grab' ;
+    // 定義與紀錄目前滑鼠 x y 座標與捲軸拉條至最左側距離
+    let pos = { top: 0, left: 0, x: 0, y: 0 } ;
+    const mouseDownHandler = Event => {
+        recommendDOM.style.cursor = 'grabbing' ;
+        pos = {
+          // 取得元素之 X 軸與最左側與上端距離
+          left: recommendDOM.scrollLeft,
+          top: recommendDOM.scrollTop,
+          // 取得滑鼠目前座標
+          x: Event.clientX,
+          y: Event.clientY,
+        };
+        // 當滑鼠按下時，開始監聽滑鼠拖動與放開事件
+        document.addEventListener('mousemove', mouseMoveHandler) ;
+        document.addEventListener('mouseup', mouseUpHandler) ;
+    };
+    const mouseMoveHandler = Event => {
+        // 滑鼠移動距離 = 滑鼠移動時的座標 - 已記錄的滑鼠座標
+        let dx = Event.clientX - pos.x ;
+        let dy = Event.clientY - pos.y ;
+
+        // 變更捲軸位置，已記錄的捲軸拉條左邊頂端位置 - 滑鼠移動距離
+        // 舉例 : 拉條初始值為 0 ，滑鼠按下往左移動 -50，相對拉條往右移動 +50
+        recommendDOM.scrollTop = pos.top - dy ;
+        recommendDOM.scrollLeft = pos.left - dx ;
+    };
+    const mouseUpHandler = () => {
+        recommendDOM.style.cursor = 'grab' ;
+        // 當滑鼠放開時，移除滑鼠移動與放開事件
+        document.removeEventListener('mousemove', mouseMoveHandler) ;
+        document.removeEventListener('mouseup', mouseUpHandler) ;
+    };
+    // 監聽滑鼠按下事件
+    recommendDOM.addEventListener('mousedown', mouseDownHandler) ;
+});
+
+// 產品類別選擇
+productsCategory.addEventListener('change', Event => {
+  let tempData = [] ;
+  if (Event.target.value === 'all') {
+    productsRender(originProductsData) ;
+  } else {
+    originProductsData.forEach(item => {
+      if (item.category === Event.target.value){
+        tempData.push(item) ;
+      }
+    })
+    productsRender(tempData) ;
+  }
+})
+
+// 購物車操作
+cartsList.addEventListener('click', Event => {
+  switch (Event.target.dataset.action) {
+    case 'down':
+      patchProduct(Event.target.value, Event.target.nextElementSibling.dataset.quantity - 1)
+      break;
+    case 'up':
+      patchProduct(Event.target.value, Event.target.previousElementSibling.dataset.quantity*1 + 1)
+      break;
+    case 'delete':
+      deleteProduct(Event.target.value)
+      break;
+    case undefined:
+      return  
+  }
+})
+
+// 購物車刪除全部
+delAll.addEventListener('click', Event => {
+  deleteProduct(Event.target.value)
+})
+
+// 驗證表單資訊
+inputs.forEach(item => {
+  item.addEventListener('change', () => {
+    item.nextElementSibling.textContent = '';
+    let errors = validate(submitOrder, constraints);
+    if (errors) {
+      hasError = true
+      Object.keys(errors).forEach(item => {
+        document.querySelector(`.${item}`).textContent =  errors[item][0].split(' ')[2]
+      })
+    } else {
+      hasError = false
+    }
+  })
+})
+
+// 送出預定表單
+submitOrder.addEventListener('submit', Event => {
+  Event.preventDefault() ;
+  postOrder(Event) ;
+})
+
+topBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  })
+})
